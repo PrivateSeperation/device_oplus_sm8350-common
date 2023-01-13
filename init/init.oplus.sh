60243a70e8f4a43796ed5b6bdc14507aa880cc94
ZirgomHaidar <zirgomhaidar17@gmail.com>
2023-01-13 14:43:09 +0530

    Force DSDS for all devices

--- init/init.oplus.sh
+++ init/init.oplus.sh
@@ -3,8 +3,5 @@
 # Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
-#
+#	
 
-if grep -qE "androidboot.rf_version=(2|4|11|13|21)" /proc/cmdline; then
-    setprop vendor.radio.multisim.config dsds
-fi
